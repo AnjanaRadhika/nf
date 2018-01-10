7a9790fc171e5b355f38cf26d5cab2e84213bdb2
Anjana <anjana.rajeevv@gmail.com>
2018-01-10 11:40:27 -0600

    activate check
    code changes to check for activate before login and change password

--- controller/homecontroller.php
+++ controller/homecontroller.php
@@ -23,11 +23,11 @@
 						$query = "SELECT * FROM `users` WHERE `username` = '".mysqli_real_escape_string($link, $_POST['name'])."'";
 						$result = mysqli_query($link, $query);
 						$query = "SELECT * FROM `users` WHERE `email` = '".mysqli_real_escape_string($link, $_POST['email'])."'";
-						$result1 = mysqli_query($link, $query);					
+						$result1 = mysqli_query($link, $query);
 						if(mysqli_num_rows($result) > 0) {
 							$error .= '<div class="alert-danger" >User Name has already been taken.</div>';
 							$_SESSION['signupsubmitted'] = false;
-						} 
+						}
 						if(mysqli_num_rows($result1) > 0) {
 								$error .= '<div class="alert-danger">Email has already been taken.</div>';
 								$_SESSION['signupsubmitted'] = false;
@@ -35,7 +35,7 @@
 							$_SESSION['email'] = $_POST['email'];
 							$_SESSION['name'] = $_POST['name'];
 							$_SESSION['password'] = $_POST['password'];
-							$_SESSION['signupsubmitted'] = true;										
+							$_SESSION['signupsubmitted'] = true;
 						}
 					}
 				}
@@ -43,18 +43,23 @@
 				if(array_key_exists('forgotemail', $_POST)) {
 					if($_POST['forgotemail']==""){
 						$forgotpwderror .= '<div class="alert-danger" >Please enter the email address.</div>';
-						$_SESSION['forgotpwdsubmitted'] = false;					
+						$_SESSION['forgotpwdsubmitted'] = false;
 					}else {
-						$query = "SELECT `username` FROM `users` WHERE `email` = '".mysqli_real_escape_string($link, $_POST['forgotemail'])."'";
+						$query = "SELECT `username`,`activate` FROM `users` WHERE `email` = '".mysqli_real_escape_string($link, $_POST['forgotemail'])."'";
 						$result = mysqli_query($link, $query);
 						if(mysqli_num_rows($result) > 0) {
 							$row=mysqli_fetch_array($result);
-							$_SESSION['name'] = $row['username'];
-							$_SESSION['forgotemail'] = $_POST['forgotemail'];
-							$_SESSION['forgotpwdsubmitted'] = true;
+							if($row['activate']) {
+								$_SESSION['name'] = $row['username'];
+								$_SESSION['forgotemail'] = $_POST['forgotemail'];
+								$_SESSION['forgotpwdsubmitted'] = true;
+							} else {
+								$forgotpwderror .= '<div class="alert-danger">You need to get your account activated. Your account activation link has been sent to your registered email address.</div>';
+								$_SESSION['forgotpwdsubmitted'] = false;
+							}
 						} else {
 							$forgotpwderror .= '<div class="alert-danger">Please enter a valid email address.</div>';
-							$_SESSION['forgotpwdsubmitted'] = false;						
+							$_SESSION['forgotpwdsubmitted'] = false;
 						}
 					}
 				}
@@ -72,19 +77,24 @@
 						$result = mysqli_query($link, $query);
 						if(mysqli_num_rows($result) > 0) {
 							$row=mysqli_fetch_array($result);
-							$hash=$row['password'];
-							if(password_verify($_POST['password'], $hash)) {
-								$_SESSION['name'] = $row['username'];
-								$_SESSION['email'] = $_POST['username'];
-								$_SESSION['signedin'] = true;
+							if($row['activate']) {
+									$hash=$row['password'];
+									if(password_verify($_POST['password'], $hash)) {
+										$_SESSION['name'] = $row['username'];
+										$_SESSION['email'] = $_POST['username'];
+										$_SESSION['signedin'] = true;
+									} else {
+										$loginerror .= '<div class="alert-danger">Invalid credentials. Please try again.</div>';
+										$_SESSION['signedin'] = false;
+									}
 							} else {
-								$loginerror .= '<div class="alert-danger">Invalid credentials. Please try again.</div>';								
-								$_SESSION['signedin'] = false;
-							}
+										$loginerror .= '<div class="alert-danger">You need to get your account activated. Your account activation link has been sent to your registered email address.</div>';
+										$_SESSION['signedin'] = false;
+								}
 						} else {
-							$loginerror .= '<div class="alert-danger">Please enter a valid email address.</div>';	
+							$loginerror .= '<div class="alert-danger">Please enter a valid email address.</div>';
 							$_SESSION['signedin'] = false;
-						}						
+						}
 					}
 				}
 			}
@@ -92,21 +102,21 @@
 
 		 if(isset($_SESSION['signupsubmitted'])){
 			 if($_SESSION['signupsubmitted'] === true) {
-					header("Location:signupsubmit.php");			 
+					header("Location:signupsubmit.php");
 			 } else {
 					$script =  "<script>$('#signupdiv').modal('show')</script>"; // Show modal
 			 }
 		 } else if(isset($_SESSION['forgotpwdsubmitted'])){
 			 if($_SESSION['forgotpwdsubmitted'] === true) {
-					header("Location:forgotpwdsubmit.php");			 
+					header("Location:forgotpwdsubmit.php");
 			 } else {
 					$script =  "<script>$('#forgotpassworddiv').modal('show')</script>"; // Show modal
-			 }			 
+			 }
 		 } else if(isset($_SESSION['signedin'])) {
 			 if($_SESSION['signedin'] === true) {
 				 echo("<script>console.log('signed in')</script>");
 			 } else {
-					$script =  "<script>$('#signindiv').modal('show')</script>"; // Show modal				 
+					$script =  "<script>$('#signindiv').modal('show')</script>"; // Show modal
 			 }
 		 }
 		mysqli_close($link);
